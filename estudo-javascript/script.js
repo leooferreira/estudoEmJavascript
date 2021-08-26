aa7c615b00ed08c55ac1b008bce3c0cc0e6f85ba
leonardo <leooferreira07@gmail.com>
2021-08-26 20:51:57 -0300

    Exercício-2 em JavaScript utilizando function, arrays e objetos

--- estudo-javascript/script.js
+++ estudo-javascript/script.js
@@ -1,4 +1,4 @@
-// Exercício em JavaScript
+// Exercício-1 em JavaScript
 
 var car = {
     marca: 'fiat',
@@ -38,7 +38,7 @@ console.log(car.obterMarcaModelo());
 
 car.adicionarPessoas = function(numeroPessoas) {
     var totalPessoas = car.quantidadePessoas + numeroPessoas;
-    if(car.quantidadePessoas === car.assentos) {
+    if(car.quantidadePessoas === car.assentos && totalPessoas >= car.assentos) {
         return `O carro já está lotado!`
     }
 
@@ -52,5 +52,55 @@ car.adicionarPessoas = function(numeroPessoas) {
         return `Já temos ${totalPessoas} pessoas no carro!`;
 };
 
+// Exercício-2 em JavaScript
 
+let myarray = [10, 'leonardo', null, {a: 1}, function(){}];
+
+function myFunction(arr) {
+    return arr;
+}
+
+console.log(myFunction(myarray)[1]);
+
+function myFunction2(arr, index){
+    return arr[index];
+}
+
+let myarray2 = ['ninja', 52.4, true, [1, 2, 'leonardo'], {b: 2}];
+console.log(myFunction2(myarray2, 0));
+console.log(myFunction2(myarray2, 1));
+console.log(myFunction2(myarray2, 2));
+console.log(myFunction2(myarray2, 3));
+console.log(myFunction2(myarray2, 4));
+
+function book(bookName) {
+    let allBooks = {
+        'Guia de Html e Css':{
+            quantidadePaginas:333,
+            autor:'Jhon',
+            editora:'ebook'
+        },
+        'Introdução ao HTML5':{
+            quantidadePaginas:456,
+            autor:'Eric',
+            editora:'eletronicbook'
+        },
+        'Javascript do zero':{
+            quantidadePaginas:179,
+            autor:'Tony',
+            editora:'pagetech'
+        } 
+
+    };
+
+    return !bookName ? allBooks : allBooks[bookName];
+}
+
+console.log(book());
+let bookName = 'Guia de Html e Css';
+console.log(`O livro ${bookName} tem ${book(bookName).quantidadePaginas} páginas!`);
+
+console.log(`O autor do livro ${bookName} é ${book(bookName).autor}.`);
+
+console.log(`O livro ${bookName} foi publicado pela editora ${book(bookName).editora}.`);
 
